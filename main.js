e38d8ee4a2a801e3d7566d5640919e1ae18d5cc8
Pedro Loma <delalomapedro@gmail.com>
2023-03-21 20:45:01 +0100

    color

--- main.js
+++ main.js
@@ -2,14 +2,21 @@
 
 const colors = ["blue", "pink", "red", "brown", "yellow", "purple"];
 
-
 const getColors = async () => {
-    for (let color of colors) {
-        const li = document.createElement('li');
-        li.textContent = color;
-        document.querySelector('ul').append(li);
-    }
-
-}
+  for (let color of colors) {
+    const li = document.createElement("li");
+    li.textContent = color;
+    document.querySelector("ul").append(li);
+  }
+};
 
 getColors();
+
+const cambiarFondo = () => {
+  for (let color of colors) {
+    const fondo = document.querySelectorAll("li");
+    fondo.style.backgroundColor = color;
+  }
+};
+
+cambiarFondo();
